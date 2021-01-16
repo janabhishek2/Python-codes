bf93ba2e2826baa64a5301a46383ee67db1279e4
Abhishek Jan <janabhishek2@gmail.com>
2021-01-16 07:34:17 +0530

    Basics String

---
+++ strings.py
@@ -0,0 +1,12 @@
+course = "  python programming"  # sample string
+# These methods return another string
+
+# all variables are objects in python
+
+
+print(course.upper())
+print(course.title())
+print(course.strip())
+print(course.find("pro"))  # index of pro
+print(course.replace("pro", "hello"))  # replace pro with hello
+print("pro" in course)  # return -boolean (expression)
